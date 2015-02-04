66d65ada8c5f0b9ac73c2f9f8a41af16935c1da6
s9iper1 <s9iper1@gmail.com>
2015-02-05 01:05:32 +0500

    added DB add one on each click

--- app/src/main/java/nonameyetsoft/com/torch/MainActivity.java
+++ app/src/main/java/nonameyetsoft/com/torch/MainActivity.java
@@ -1,13 +1,16 @@
 package nonameyetsoft.com.torch;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity  {
@@ -18,6 +21,11 @@ public class MainActivity extends Activity  {
     Flashlight flashlight;
     Helpers helpers;
     RelativeLayout layout;
+//creating a string for DB
+    public static final String NUM = "number";
+    SharedPreferences setting;
+    public static int counter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +33,9 @@ public class MainActivity extends Activity  {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
         initializeClasses();
+//DB edited by bilal
+        setting = PreferenceManager.getDefaultSharedPreferences(this);
 
         if(!helpers.isFlashlightAvailable(this)) {
             helpers.showErrorDialog(this);
@@ -40,6 +49,23 @@ public class MainActivity extends Activity  {
                 if (!flashlight.isFlashOn()) {
                     flashlight.turnOnFlash();
                     layout.setBackgroundColor(Color.WHITE);
+                    // working with  shared preference starts edited by bilal
+                    int val =  setting.getInt(NUM , 0);
+                    if (val == 0){
+                        counter = val+1;
+                    }
+                    else
+                    {
+                        counter = val+1;
+                    }
+                    SharedPreferences.Editor  editor;
+                    editor = setting.edit();
+                    int inputVal = counter ;
+                    editor.putInt(NUM, inputVal);
+                    editor.apply();
+                    int val2 =  setting.getInt(NUM , 0);
+                   // System.out.println(val2);
+                    // ends edited by bilal
                 } else {
                     flashlight.turnOffFlash();
                     layout.setBackgroundColor(Color.BLACK);
