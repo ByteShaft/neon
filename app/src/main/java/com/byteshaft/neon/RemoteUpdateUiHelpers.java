056bdd393ec0a3587f250a45216baa61cffd99e9
Omer Akram <om26er@gmail.com>
2015-02-20 23:56:06 +0500

    add logging to widget UI update code.

--- app/src/main/java/com/byteshaft/neon/RemoteUpdateUiHelpers.java
+++ app/src/main/java/com/byteshaft/neon/RemoteUpdateUiHelpers.java
@@ -3,6 +3,7 @@ package com.byteshaft.neon;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.RemoteViews;
 
@@ -28,7 +29,8 @@ public class RemoteUpdateUiHelpers {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
         RemoteViews views = WidgetProvider.mRemoteViews;
         if (views == null) {
-            return;
+            Log.i(Flashlight.LOG_TAG, "Widget view was null, creating.");
+            views = new RemoteViews(mContext.getPackageName(), R.layout.neon_widget);
         }
 
         if (ON) {
