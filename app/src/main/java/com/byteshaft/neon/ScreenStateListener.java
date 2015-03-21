ca9a42610f4af9d37b53a8e566cfec2019b4207a
Omer Akram <om26er@gmail.com>
2015-03-21 23:12:42 +0500

    Add the screen state listener

---
+++ app/src/main/java/com/byteshaft/neon/ScreenStateListener.java
@@ -0,0 +1,47 @@
+package com.byteshaft.neon;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+
+public class ScreenStateListener extends ContextWrapper {
+
+    private BroadcastReceiver mReceiver = null;
+
+    public ScreenStateListener(Context base) {
+        super(base);
+    }
+
+    void register() {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF) && Build.MANUFACTURER
+                        .equals("HTC")) {
+                    restartTorch();
+                }
+            }
+        };
+        registerReceiver(mReceiver, filter);
+    }
+
+    void unregister() {
+        try {
+            unregisterReceiver(mReceiver);
+        } catch (IllegalArgumentException ignore) {
+
+        }
+    }
+
+    private void restartTorch() {
+        FlashlightService flashlightService = FlashlightService.getInstance();
+        if (flashlightService != null) {
+            flashlightService.stopTorch();
+            flashlightService.lightenTorch();
+        }
+    }
+}
