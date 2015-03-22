55f73fe487031f06d22db04cd2aec45b1c9d42b5
Omer Akram <om26er@gmail.com>
2015-03-22 15:58:15 +0500

    Clean logging

--- app/src/main/java/com/byteshaft/neon/WidgetReceiver.java
+++ app/src/main/java/com/byteshaft/neon/WidgetReceiver.java
@@ -3,22 +3,20 @@ package com.byteshaft.neon;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class WidgetReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        final String STARTER = "widget";
         AppGlobals.setIsWidgetTapped(true);
         Intent serviceIntent = new Intent(context, FlashlightService.class);
-        serviceIntent.putExtra("STARTER", "widget");
+        serviceIntent.putExtra("STARTER", STARTER);
         RemoteUpdateUiHelpers remoteUi = new RemoteUpdateUiHelpers(context);
 
         if (FlashlightService.isRunning()) {
-            Log.i(AppGlobals.LOG_TAG, "Turning off from widget.");
             context.stopService(serviceIntent);
         } else {
-            Log.i(AppGlobals.LOG_TAG, "Turning on from widget.");
             remoteUi.setUiButtonsOn(true);
             context.startService(serviceIntent);
         }
