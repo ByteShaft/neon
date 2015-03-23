445936ffa0bcda06cc1ffafe54ebd0342021aca0
Omer Akram <om26er@gmail.com>
2015-03-23 14:18:21 +0500

    remove whitespaces

--- app/src/main/java/com/byteshaft/neon/FlashlightService.java
+++ app/src/main/java/com/byteshaft/neon/FlashlightService.java
@@ -91,8 +91,6 @@ public class FlashlightService extends Service implements CameraInitializationLi
         Log.i(AppGlobals.LOG_TAG, "Service down.");
     }
 
-
-
     @Override
     public IBinder onBind(Intent intent) {
         return null;
